8ef75a0fcbdf6efccf1ad8960959d49032c66196
Anthony Knoll <anthonyknoll@yahoo.com>
2023-01-02 14:54:15 -0700

    Updated to make sure user was logged in

--- src/components/app.js
+++ src/components/app.js
@@ -1,50 +1,101 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-
-import PortfolioContainer from './portfolio/portfolio-container';
-import NavigationContainer from './navigation/navigation-container';
-import Home from './pages/home';
-import About from './pages/about';
-import Contact from './pages/contact';
-import Blog from './pages/blog';
-import PortfolioDetail from './portfolio/portfolio-detail';
-import Auth from "./pages/auth"
-import NoMatch from './pages/no-match';
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
-  
-  render() {
-    
-    return (
-      <div className='appcontainer'>
-       
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN"
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnSuccessfulLogin = this.handleUnSuccessfulLogin.bind(this);
+  }
+
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleUnSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  checkLoginStatus() {
+    return axios.get("https://api.devcamp.space/logged_in",{withCredentials: true})
+    .then(response => {
+      const loggedIn = response.data.logged_in
+      const loggedInStatus = thi.state.loggedInStatus
+      if (loggedIn && loggedInStatus ==="LOGGED_IN") {
+        returnloggedIn;
+      }else if(loggedIn&& loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN"
+        });
+      }else if(!loggedIn && loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+})
+  .catch(error => {
+    console.log("ERROR", error);
+  })
+  }
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+
+  render() {
+    return (
+      <div className="container">
         <Router>
           <div>
-          
             <NavigationContainer />
+
+            <h2>{this.state.loggedInStatus} </h2>
 
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route exact path="/auth" component={Auth} />
-              <Route exact path="/about-me" component={About} />
-              <Route exact path="/contact" component={Contact} />
-              <Route exact path="/blog" component={Blog} />
-              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+
+              <Route
+                path="/auth"
+                render={props => (
+                  <Auth
+                    {...props}
+                    handleSuccessfulLogin={this.handleSuccessfulLogin}
+                    handleUnsuccessfulLogin={this.handleUnsuccessfulLogin}
+                  />
+                )}
+              />
+
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
               <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
-
-        
-          
-      </div>  
+      </div>
     );
   }
 }
